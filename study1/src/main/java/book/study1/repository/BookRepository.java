5196b327c380aba709d612ca42545d5b42f2d266
omerjava <ofk45@hotmail.com>
2026-02-24 15:44:42 +0100

    remove all

--- study1/src/main/java/book/study1/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package book.study1.repository;
-
-import book.study1.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
